a738a7c39ab1890ed97dfa5b7f8e239d1c221ae4
limajeferson <jefersonlima@outlook.com>
2022-01-25 14:30:07 -0300

    Initial commit

---
+++ bin/desafio_soma.dart
@@ -0,0 +1,5 @@
+import 'package:desafio_soma/desafio_soma.dart' as desafio_soma;
+
+void main(List<String> arguments) {
+  print('Hello world: ${desafio_soma.calculate()}!');
+}
---
+++ test/desafio_soma_test.dart
@@ -0,0 +1,8 @@
+import 'package:desafio_soma/desafio_soma.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
